c448548dd5c9fc763facb11147a0f93e1dfe78bc
alejandratemes <116831450+alejandratemes@users.noreply.github.com>
2023-05-03 15:01:47 +0200

    ejercicio1

---
+++ index.js
@@ -0,0 +1,13 @@
+const getJoke = async () => {
+    try {
+      const randomJoke = await fetch("https://icanhazdadjoke.com/", {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      const json = await randomJoke.json();
+      document.getElementById("jokeText").innerHTML = json.joke;
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
