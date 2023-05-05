94bd6f9fef9447ba33608acdc8d0ef58c7ac764d
alejandratemes <116831450+alejandratemes@users.noreply.github.com>
2023-05-05 13:41:44 +0200

    ejercicio3

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+const showButtons = () => document.querySelectorAll("[rate]").forEach(element => element.style.display = "inline-block");
+
+let answer;
 const getJoke = async () => {
-    try {
       const randomJoke = await fetch("https://icanhazdadjoke.com/", {
         headers: {
           Accept: "application/json",
         },
       });
-      const json = await randomJoke.json();
-      document.getElementById("jokeText").innerHTML = `"${json.joke}"`;
-    } catch (err) {
-      console.log(err.message);
-    }
+      const random = await randomJoke.json();
+      document.getElementById("jokeText").innerHTML = `"${random.joke}"`;
+      answer = random.joke;
+    showButtons()
   };
+
+let reportJokes = [];
+const addRating = score => {
+  let report = {
+    joke: answer,
+    score: score,
+    date: new Date().toISOString()
+  };
+  reportJokes.push(report);
+  console.log(reportJokes)
+}
