79fe745779876263f96e21b68f16bdace7166b05
alejandratemes <116831450+alejandratemes@users.noreply.github.com>
2023-05-08 23:57:02 +0200

    ejercicio4

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const showButtons = () => document.querySelectorAll("[rate]").forEach(element => element.style.display = "inline-block");
-
 let answer;
 const getJoke = async () => {
-      const randomJoke = await fetch("https://icanhazdadjoke.com/", {
-        headers: {
-          Accept: "application/json",
-        },
-      });
-      const random = await randomJoke.json();
-      document.getElementById("jokeText").innerHTML = `"${random.joke}"`;
-      answer = random.joke;
-    showButtons()
-  };
+  const randomJoke = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+    Accept: "application/json",
+    },
+  });
+  const random = await randomJoke.json();
+  document.getElementById("jokeText").innerHTML = `"${random.joke}"`;
+  answer = random.joke;
+  showButtons()
+};
 
 let reportJokes = [];
 const addRating = score => {
@@ -23,3 +22,11 @@ const addRating = score => {
   reportJokes.push(report);
   console.log(reportJokes)
 }
+
+const getWeather = async () => {
+  const weather = await fetch ("https://api.openweathermap.org/data/2.5/weather?lat=41.3828939&lon=2.1774322&appid=878d505b1bafe0572eb03ee5e3a85481")
+  const response = await weather.json()
+  const currentWeather = response.weather[0].description
+  document.getElementById("weatherAPI").innerHTML = currentWeather
+}
+getWeather()
